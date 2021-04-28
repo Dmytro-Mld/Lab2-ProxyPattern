4dcbfc8baf76260be48b739c5992f81b3f2bcb16
Dmytro Moldovan <79337586+Dmytro-Mld@users.noreply.github.com>
2021-04-29 01:53:38 +0300

    Proxy1

---
+++ ProxyPattern/ProxyPattern/Class Proxy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyPattern
+{
+    class Proxy : Subject
+    {
+        RealSubject realSubject = new RealSubject();
+        public override void Request()
+        {
+            realSubject.Request();
+        }
+        public Proxy(RealSubject Subject)
+        {
+            realSubject = Subject;
+        }
+    }
+}
---
+++ ProxyPattern/ProxyPattern/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProxyPattern;
+        
+class Client
+{
+    void Main()
+    {
+        RealSubject a = new RealSubject();
+        Subject subject = new Proxy(a);
+        subject.Request();
+    }
+}
+
+
